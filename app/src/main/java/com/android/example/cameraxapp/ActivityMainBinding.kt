fa4f7404a934bfbea36557209e9eaa23d0dc8b96
ddo0S2 <syyh0103@naver.com>
2024-07-17 17:43:49 +0900

    MY_Capstone_Camera

---
+++ app/src/main/java/com/android/example/cameraxapp/ActivityMainBinding.kt
@@ -0,0 +1,25 @@
+package com.android.example.cameraxapp
+
+import android.view.LayoutInflater
+
+class ActivityMainBinding {
+    val videoCaptureButton: Any
+        get() {
+            TODO()
+        }
+    val imageCaptureButton: Any
+        get() {
+            TODO()
+        }
+    val root: Any
+        get() {
+            TODO()
+        }
+
+    companion object {
+        fun inflate(layoutInflater: LayoutInflater): ActivityMainBinding {
+            TODO("Not yet implemented")
+        }
+    }
+
+}
