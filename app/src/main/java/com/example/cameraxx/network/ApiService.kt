9398074875c41f54fdff35dfe8a5d51051d02c54
ddo0S2 <syyh0103@naver.com>
2024-08-26 15:31:35 +0900

    수정

--- app/src/main/java/com/example/cameraxx/network/ApiService.kt
+++ app/src/main/java/com/example/cameraxx/network/ApiService.kt
@@ -5,8 +5,12 @@ import retrofit2.Call
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import com.google.gson.JsonObject
+import retrofit2.http.GET
 
 interface ApiService {
+    @GET("/result")
+    fun getPrediction(): Call<JsonObject>
     @Multipart
     @POST("/upload")  // Flask 서버에서 정의한 업로드 경로
     fun uploadImage(@Part file: MultipartBody.Part): Call<String>
