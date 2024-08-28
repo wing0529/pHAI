9049e26621880acd32c999dbc0ca686c36d67e40
nanana <124423751+wing0529@users.noreply.github.com>
2024-08-28 22:43:10 +0900

    Delete app directory

--- app/src/main/java/com/example/cameraxx/network/PredictionResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cameraxx.network
-
-import com.google.gson.annotations.SerializedName
-
-data class PredictionResponse(
-    @SerializedName("prediction") val prediction: String
-)
